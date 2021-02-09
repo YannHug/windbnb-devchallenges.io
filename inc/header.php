8c20cd2f60c3e26503bacf37e97726ab66fe556a
Yannick HUGUES <yannhweb@gmail.com>
2021-02-09 14:13:37 +0100

    create list with city

--- inc/header.php
+++ inc/header.php
@@ -1,12 +1,23 @@
+<?php
+require ('functions/create-cards.php');
+require ('datas/stays.php');
+
+?>
+
 <header>
-    <nav>
-      <h1>
-        <img src="img/logo.png" alt="logo for WindBnB">
-      </h1>
-      <form action="" method="get">
-        <label for="site-search">Search the site:</label>
-        <input type="search" id="site-search" name="site-search" aria-label="Search through site content">
-        <button>Search</button>
-      </form>
-    </nav>
-  </header>
+  <nav>
+    <h1>
+      <img src="img/logo.png" alt="logo for WindBnB">
+    </h1>
+    <form action="" method="get">
+      <label for="search">Search the site:</label>
+      <select name="search" id="search">
+        <option value="">--Please choose an option--</option>
+        <?php foreach ($stays as $rentalIndex => $rentalInfos): ?>
+        <option value="<?= $rentalInfos['city']; ?>"><?= $rentalInfos['city']; ?></option>
+        <?php endforeach ?>
+      </select>
+      <button>Search</button>
+    </form>
+  </nav>
+</header>
