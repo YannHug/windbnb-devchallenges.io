51cb6336d6f47e5752e931ede5ad7741706bffcf
Yannick HUGUES <yannhweb@gmail.com>
2021-02-09 15:57:41 +0100

    create function for create list of city

---
+++ functions/create-list.php
@@ -0,0 +1,28 @@
+<?php
+
+function create_list($stays)
+{
+  $citys = [];
+  foreach ($stays as $rentalInfos) {
+    if (!isAlreadyInList($rentalInfos, $citys)) {
+      $citys[] = $rentalInfos['city'];
+    }
+  }
+  list_city($citys);
+};
+
+
+function isAlreadyInList($rentalInfos, $citys)
+{
+    if (in_array($citys, $rentalInfos)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function list_city($citys) {
+  foreach($citys as $city ){
+    echo '<option value="<?=' . $city . '">' . $city . '</option>';
+  }
+};
--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,6 @@
 <?php
 require ('functions/create-cards.php');
+require ('functions/create-list.php');
 require ('datas/stays.php');
 
 ?>
@@ -13,9 +14,7 @@ require ('datas/stays.php');
       <label for="search">Search the site:</label>
       <select name="search" id="search">
         <option value="">--Please choose an option--</option>
-        <?php foreach ($stays as $rentalIndex => $rentalInfos): ?>
-        <option value="<?= $rentalInfos['city']; ?>"><?= $rentalInfos['city']; ?></option>
-        <?php endforeach ?>
+        <?=create_list($stays)?>
       </select>
       <button>Search</button>
     </form>
