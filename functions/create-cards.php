5d74341a4e9438b0b1f38cf93c34bfe8dfb89fa7
Yannick HUGUES <yannhweb@gmail.com>
2021-02-09 16:13:40 +0100

    remove useless variable

--- functions/create-cards.php
+++ functions/create-cards.php
@@ -1,7 +1,7 @@
 <?php
 
 function get_location($stays, $location) {
-    foreach ($stays as $rentalIndex => $rentalInfos) {
+    foreach ($stays as $rentalInfos) {
       if (empty($location)){
         include('inc/cards.php');
       }elseif (ucfirst($location) === $rentalInfos['city']){
