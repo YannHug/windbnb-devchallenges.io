0272ca585511649ae8d0d2827cb9f9613ffbde08
Yannick HUGUES <yannhweb@gmail.com>
2021-02-09 14:13:08 +0100

    simplification du get

--- functions/get.php
+++ functions/get.php
@@ -1,11 +1,7 @@
 <?php
 
-function get_location($stays, $location) {
-  foreach ($stays as $rentalIndex => $rentalInfos) {
-    if (empty($location)){
-      include('inc/cards.php');
-    }elseif (ucfirst($location) === $rentalInfos['city']){
-      include('inc/cards.php');
-    }
-  }
+if(!empty($_GET)){
+  $location = $_GET['search'];
 }
+
+
