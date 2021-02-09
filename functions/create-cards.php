a0544849daf01c7a54723d7f74f25c200064448d
Yannick HUGUES <yannhweb@gmail.com>
2021-02-09 14:12:51 +0100

    create function create cards

---
+++ functions/create-cards.php
@@ -0,0 +1,11 @@
+<?php
+
+function get_location($stays, $location) {
+    foreach ($stays as $rentalIndex => $rentalInfos) {
+      if (empty($location)){
+        include('inc/cards.php');
+      }elseif (ucfirst($location) === $rentalInfos['city']){
+        include('inc/cards.php');
+      }
+    }
+  }
